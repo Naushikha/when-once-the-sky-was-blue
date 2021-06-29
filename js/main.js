bc559facd9b8b5f0d9067cad81212b930ba90bab
Naushikha <naushikha.jayawickrama@gmail.com>
2021-06-29 11:49:35 +0530

    Improved loading screen

--- js/main.js
+++ js/main.js
@@ -7,6 +7,21 @@ const progressBar = document.getElementById("progress-bar");
 const loadingOverlay = document.getElementById("loading-overlay");
 const startButton = document.getElementById("start-button");
 
+let loadingState = true;
+let phrases = [
+  "The experience is loading...",
+  "Get comfortable.",
+  "No distractions.",
+  "We are getting things ready...",
+  "Props are being setup...",
+  "Adjusting placements...",
+  "Stage area is ready.",
+  "Testing lighting...",
+  "Performing sound checks...",
+  "Narrator is warming up...",
+  "Anytime now...",
+  "Patience is a virtue.",
+];
 // Loading manager
 const manager = new THREE.LoadingManager();
 manager.onProgress = function (item, loaded, total) {
@@ -16,15 +31,17 @@ manager.onProgress = function (item, loaded, total) {
 };
 
 manager.onLoad = function () {
+  loadingState = false;
   progress.style.display = "none";
   loadingOverlay.style.visibility = "visible";
   startButton.style.visibility = "visible";
-  
+  startButton.style.animation = "fadein 5s";
+
   // Skip play for now
-  lobby.render();
-  lobby.play();
-  startButton.style.visibility = "hidden";
-  loadingOverlay.style.visibility = "hidden";
+  // lobby.render();
+  // lobby.play();
+  // startButton.style.visibility = "hidden";
+  // loadingOverlay.style.visibility = "hidden";
 
   startButton.addEventListener("click", () => {
     lobby.render();
@@ -33,6 +50,26 @@ manager.onLoad = function () {
     loadingOverlay.style.visibility = "hidden";
   });
 };
+
+let phraseCount = 0;
+function showPhrases() {
+  const phrase = document.getElementById("phrase");
+  if (!loadingState) {
+    phrase.visibility = false;
+    return;
+  }
+  if (phraseCount == phrases.length) {
+    phraseCount = 0;
+  }
+  phrase.innerHTML = phrases[phraseCount];
+  phrase.classList.remove("run-animation");
+  void phrase.offsetWidth;
+  phrase.classList.add("run-animation");
+  phraseCount++;
+  setTimeout(showPhrases, 5000);
+}
+
+setTimeout(showPhrases, 18000);
 
 // Setup the renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
