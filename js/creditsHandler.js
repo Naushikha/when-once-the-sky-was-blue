ef63125ec118ad0594f4e9eb2df6cd248aba777d
Naushikha <naushikha.jayawickrama@gmail.com>
2021-07-04 02:59:12 +0530

    Added in credits feature

---
+++ js/creditsHandler.js
@@ -0,0 +1,91 @@
+// Simple credits loader
+class creditsHandler {
+  constructor(titleElemID, nameElemID) {
+    this.title = document.getElementById(titleElemID);
+    this.name = document.getElementById(nameElemID);
+    this.animation = new TWEEN.Group(); // Animation
+  }
+  async load(url) {
+    function parseCredits(csv) {
+      const lines = csv.split("\n");
+      let credits = [];
+      for (let line of lines) {
+        const parts = line.split(",");
+        credits.push({ title: parts[0], name: parts[1] });
+      }
+      return credits;
+    }
+    return fetch(url)
+      .then((response) => response.text())
+      .then((csv) => {
+        let credits = parseCredits(csv);
+        this.credits = credits;
+        return credits;
+      });
+  }
+  showCredit() {
+    const quarterTime = 5000 / 4; // Duration of a credit display
+    var posVec1 = {
+      o: 0,
+    };
+    var endVec1 = {
+      o: 1,
+    };
+    var fadeIn = new TWEEN.Tween(posVec1, this.animation).to(
+      endVec1,
+      quarterTime
+    );
+    fadeIn.onUpdate(
+      function () {
+        this.title.style.opacity = posVec1.o;
+        this.name.style.opacity = posVec1.o;
+      }.bind(this)
+    );
+    fadeIn.onComplete(function () {
+      posVec1.o = 1;
+      endVec1.o = 0;
+      fadeOut.start();
+    });
+    var fadeOut = new TWEEN.Tween(posVec1, this.animation).to(
+      endVec1,
+      quarterTime
+    );
+    fadeOut.delay(quarterTime * 2); // Show the subthis.title for this long
+    fadeOut.onUpdate(
+      function () {
+        this.title.style.opacity = posVec1.o;
+        this.name.style.opacity = posVec1.o;
+      }.bind(this)
+    );
+    fadeOut.onComplete(
+      function () {
+        if (this.currentCredit < this.credits.length - 1) {
+          this.currentCredit += 1;
+          this.title.innerHTML = this.credits[this.currentCredit].title;
+          this.name.innerHTML = this.credits[this.currentCredit].name;
+          setTimeout(this.showCredit.bind(this), quarterTime);
+        } else {
+          cancelAnimationFrame(this.renderID);
+          this.currentCredit = 0;
+          this.renderState = false;
+        }
+      }.bind(this)
+    );
+    fadeIn.easing(TWEEN.Easing.Quadratic.InOut);
+    fadeOut.easing(TWEEN.Easing.Quadratic.InOut);
+    fadeIn.start();
+  }
+  playCredits() {
+    this.currentCredit = 0; // Start from this subtitle
+    this.title.innerHTML = this.credits[0].title;
+    this.name.innerHTML = this.credits[0].name;
+    this.render();
+    this.showCredit();
+  }
+  render() {
+    this.renderID = requestAnimationFrame(this.render.bind(this));
+    this.animation.update();
+  }
+}
+
+export { creditsHandler };
